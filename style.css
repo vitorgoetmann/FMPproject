91f3281d64e5d761429970a80e305053743df501
vitorgoetmann <144917976+vitorgoetmann@users.noreply.github.com>
2023-09-20 10:06:24 -0300

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,134 @@
 
+
+:root {
+    --bege1: #AB9B6A;
+    --bege2: #988752;
+    --branco: #FFFFFF;
+    --preto: #000000;
+
+}
+
+#box {
+    background-color: var(--branco);
+    width: 400px;
+    border: 1px solid var(--bege1);
+    border-radius: 2px;
+    padding: 40px;
+    margin: 20px;
+}
+
+
+h1{
+    align-items: center;
+    flex-direction: column;
+    display: flex;
+
+}
+body {
+    font-family: arial light;
+    font-size: 13px;
+    background-color: var(--branco);
+    color: var(--preto);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+}
+
+.content {
+    font-family:"open sans", "Helvetica Neue", Helvetica, Arial, sans-serif ;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+}
+
+.content--add-item {
+    font-family:"open sans", "Helvetica Neue", Helvetica, Arial, sans-serif ;
+    display: flex;
+    align-items: center;
+    row-gap: 4px;
+    flex-wrap: wrap;
+}
+
+#input-new-task,
+#btn-new-task{
+    font-family:"open sans", "Helvetica Neue", Helvetica, Arial, sans-serif ;
+    border-width: 1px;
+    border-style: solid;
+    border-color: var(--bege1);
+}
+
+#input-new-task {
+    width: 340px;
+    height: 25px;
+    border-radius: 2px;
+    outline-color: var(--bege2);
+}
+
+#btn-new-task {
+    background-color: var(--bege1);
+    color: var(--branco);
+    width: 35px;
+    height: 30px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: .3s;
+    margin: 5px;
+
+}
+
+#btn-new-task:hover {
+    background-color:var(--bege2)
+}
+
+#to-do-list {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 0;
+  
+    
+}
+
+#to-do-list li {
+    list-style-type: none;
+    font-size: 15px;
+    text-transform: capitalize;
+    display: flex;
+    justify-content: space-between;
+    font-size: 14px;
+}
+
+#btn-ok {
+    border: none;
+    background-color: var(--bege1);
+    color: white;
+    border-radius: 50%;
+    height: 25px;
+    width: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 4px;
+    padding: .1px .2px;
+    cursor: pointer;
+    transition: .3s;
+}
+
+#btn-ok:hover {
+    background-color: var(--bege2);
+}
+
+.line-through {
+    text-decoration: line-through;    
+}
+
+.icon-size {
+    font-size: 10px;
+}
+
+.flex {
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+}
